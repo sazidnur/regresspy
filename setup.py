092b1e0f7a01d5345da79c262216205ccc1ca52c
Sazid Nur Ratul <sazid35-346@diu.edu.bd>
2021-09-02 18:44:48 +0600

    all file updated with correct answer

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='regresspy',
+version='0.2.0',
+description='A very specific package for evaluate the values of gradient descent, loss and regression. ',
+url='#',
+author='Sazid Nur Ratul',
+author_email='sazidnurratul2@gmail.com',
+license='MIT',
+packages=['regresspy'],
+zip_safe=False)
